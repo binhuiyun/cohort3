292d19c803f3607676fdbac825cef467559687c0
baby bin <binhuiyun@gmail.com>
2026-08-18 00:34:38 -0700

    crud complete

--- betteruptime/packages/store/index.ts
+++ betteruptime/packages/store/index.ts
@@ -3,7 +3,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+export const prismaClient = new PrismaClient({ adapter });
 
-export { PrismaClient } from "./generated/prisma/client";
-export default prisma;
+
