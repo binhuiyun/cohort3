78bf2e9f2c5c7da274a2e57aaca181212a589d59
baby bin <binhuiyun@gmail.com>
2026-09-02 17:54:23 -0700

    middleware

--- brainly/src/middleware.ts
+++ brainly/src/middleware.ts
@@ -5,18 +5,18 @@ import { JWT_PASSWORD } from "./config.js";
 export const userMiddleware = (req: Request, res: Response, next: NextFunction) =>{
     const header = req.headers["authorization"];
     const decoded = jwt.verify(header as string, JWT_PASSWORD)
-    if (decoded){
+   if (decoded) {
+        if (typeof decoded === "string") {
+            res.status(403).json({
+                message: "You are not logged in"
+            })
+            return;    
+        }
+        req.userId = (decoded as JwtPayload).id;
+        next()
+    } else {
         res.status(403).json({
             message: "You are not logged in"
         })
-        return;
-    
-    req.userId = decoded.id;
-    next()
     }
-else{
-    res.status(403).json({
-        message: "You are not logged in"
-    })
- 
 }
