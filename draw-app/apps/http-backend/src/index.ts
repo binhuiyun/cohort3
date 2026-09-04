5c6627f0cebc7e727fb9e8cf15be27243b4585b4
baby bin <binhuiyun@gmail.com>
2026-09-03 20:51:12 -0700

    structure file

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -2,4 +2,44 @@ import express from "express";
 
 const app = express();
 
+app.post("/api/v1/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body. password;
+
+    try{
+        await UserModel.create({
+            username,
+            password
+        })
+        res.json({
+            message: "User signed up"
+        })
+    }catch(e){
+        res.status(411).json({
+            message:" User already exists"
+        })
+    }
+    
+})
+
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body. password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser){
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({token})
+    }else{
+        res.status(403).json({
+            message: "incorrect credentials"
+        })
+    }
+
+})
 app.listen(3001)
